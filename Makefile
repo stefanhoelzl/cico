13f2351681755598606e2dbb2c77275b4d68a288
Stefan Hoelzl <stefan.hoelzl@posteo.de>
2018-07-08 18:02:15 +0200

    [toolchain] setup.py added

---
+++ Makefile
@@ -0,0 +1,24 @@
+.PHONY: default
+default: all;
+
+.PHONY: tests
+tests:
+	PYTHONPATH=. pytest tests
+
+.PHONY: release.build
+release.build:
+	python setup.py sdist bdist_wheel
+
+.PHONY: release.upload
+release.upload: release.build
+	twine upload ${OPTS} dist/*
+
+.PHONY: env.install
+env.install:
+	python -m pip install -r requirements.txt
+
+.PHONY: all
+all: tests docs.build release.build
+
+.PHONY: ci
+ci: all
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name='cico',
+    version='0.0.0',
+    description='stores results created during a CI in a special git branch',
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Version Control :: Git'
+    ],
+    keywords='ci travis git badge',
+    url='http://github.com/stefanhoelzl/cico',
+    author='Stefan Hoelzl',
+    author_email='stefan.hoelzl@posteo.de',
+    license='MIT',
+    packages=['cico'],
+    install_requires=[
+        'anybadge==1.1.1',
+        'GitPython==2.1.10',
+        'CairoSVG==2.1.3',
+    ],
+    include_package_data=True,
+    zip_safe=False
+)
