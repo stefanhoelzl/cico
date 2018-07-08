b5b5756a33e6ad54da8a75ed46667f40365d7913
Stefan Hoelzl <stefan.hoelzl@posteo.de>
2018-07-08 23:20:18 +0200

    [internal] switched to production pypi

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.5',
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: POSIX :: Linux',
         'Topic :: Software Development',
