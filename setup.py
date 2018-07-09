85a8990919d5722adc0956facbd470f9730d1de7
Stefan Hoelzl <stefan.hoelzl@baltech.de>
2018-07-09 17:08:14 +0200

    [release] v0.1.8

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='cico',
-    version='0.1.7',
+    version='0.1.8',
     description='deploy CI results to git',
     long_description=readme(),
     long_description_content_type="text/markdown",
