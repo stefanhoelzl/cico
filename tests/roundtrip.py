5f7faac4f14a324d46c7bc5496f692a177bacc90
Stefan Hoelzl <stefan.hoelzl@baltech.de>
2018-07-09 14:46:57 +0200

    [tests] run roundtrip tests on ci-results repo

--- tests/roundtrip.py
+++ tests/roundtrip.py
@@ -14,20 +14,22 @@ with TemporaryDirectory() as temp:
     temp_file.write_text(current_time)
 
     commit_result = TravisCI(
-        repo=GitHub("stefanhoelzl", "cico", os.environ.get("GITHUB_TOKEN", "")),
-        branch="testing",
+        repo=GitHub("stefanhoelzl", "ci-results",
+                    os.environ.get("GITHUB_TOKEN", "")),
+        branch="cico-testing",
         results=[
             File(temp_file)
         ]
     ).commit(no_ci_push=True)
 
 ssl._create_default_https_context = ssl._create_unverified_context
-REPO_URL = "https://raw.githubusercontent.com/stefanhoelzl/cico"
-URL = "{}/testing/{}/roundtrip".format(REPO_URL, commit_result["branch"])
+REPO_URL = "https://raw.githubusercontent.com/stefanhoelzl/ci-results"
+URL = "{}/cico-testing/{}/roundtrip".format(REPO_URL, commit_result["branch"])
 
 retrieved_time = ""
 while current_time != retrieved_time \
-        and float(current_time)+60 > time.time():
+        and float(current_time)+5*60 > time.time():
     urllib.request.urlcleanup()
     retrieved_time = urllib.request.urlopen(URL).read().decode("utf-8").strip()
+    time.sleep(1)
 assert current_time == retrieved_time
